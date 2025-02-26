805592640d694d7cb0ee47aa2d05abe323f61a55
RumeelHussain <35759187+RumeelHussain@users.noreply.github.com>
2025-02-26 09:37:08 +0500

    New translations sidebar.ts (French)

--- i18n/fr/docs/stakingAPI/mainnet/sidebar.ts
+++ i18n/fr/docs/stakingAPI/mainnet/sidebar.ts
@@ -2,21 +2,21 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [{
     type: "category",
-    label: "Staking Details",
+    label: "Détails du staking",
     items: [{
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-staking-summary",
-      label: "Retrieve Staking Summary",
+      label: "Récupérer le Résumé du Staking",
       className: "api-method get"
     }, {
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-btc-staking-status",
-      label: "Retrieve BTC Staking Status",
+      label: "Récupérer le Statut du Staking BTC",
       className: "api-method get"
     }, {
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-overall-staking-data-on-core-blockchain",
-      label: "Retrieve Overall Staking Data on Core Blockchain",
+      label: "Récupérer les Données Globales de Staking sur la Blockchain Core",
       className: "api-method get"
     }]
   }, {
@@ -25,12 +25,12 @@ const sidebar: SidebarsConfig = {
     items: [{
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-btc-staking-transactions",
-      label: "Retrieve BTC staking transactions",
+      label: "Récupérer les transactions de staking BTC",
       className: "api-method get"
     }, {
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-full-list-of-btc-staking-transactions",
-      label: "Retrieve Full List of BTC Staking Transactions",
+      label: "Récupérer la liste complète des transactions de staking BTC",
       className: "api-method get"
     }]
   }, {
@@ -39,17 +39,17 @@ const sidebar: SidebarsConfig = {
     items: [{
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-reward-claim-history",
-      label: "Retrieve Reward Claim History",
+      label: "Récupérer l'Historique des Réclamations de Récompenses",
       className: "api-method get"
     }, {
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-reward-distribution-of-a-round",
-      label: "Retrieve Reward Distribution of a Round",
+      label: "Récupérer la Distribution des Récompenses d'un Round",
       className: "api-method get"
     }, {
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-collected-and-pending-rewards",
-      label: "Retrieve Collected and Pending Rewards",
+      label: "Récupérer les Récompenses Collectées et en Attente",
       className: "api-method get"
     }]
   }, {
@@ -58,23 +58,23 @@ const sidebar: SidebarsConfig = {
     items: [{
       type: "doc",
       id: "stakingAPI/mainnet/retrieve-current-dual-staking-grade",
-      label: "Retrieve Current Dual Staking Grade",
+      label: "Récupérer le Niveau Actuel de Dual Staking",
       className: "api-method get"
     }]
   }, {
     type: "doc",
     id: "stakingAPI/mainnet/retrieve-latest-staking-round",
-    label: "Latest Staking Round",
+    label: "Dernier Round de Staking",
     className: "api-method get"
   }, {
     type: "doc",
     id: "stakingAPI/mainnet/retrieve-validator-details",
-    label: "Validator Details",
+    label: "Détails des Validateurs",
     className: "api-method get"
   }, {
     type: "doc",
     id: "stakingAPI/mainnet/submit-redeem-script-for-staking-transaction",
-    label: "Submit RedeemScript",
+    label: "Soumettre le RedeemScript",
     className: "api-method post"
   }]
 };
