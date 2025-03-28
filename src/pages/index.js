c205a5216242f02be88cd5d1a2aa4542fd3754a6
RumeelHussain <35759187+RumeelHussain@users.noreply.github.com>
2025-03-28 15:15:06 +0500

    Update re-route url

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,4 @@ import Layout from '@theme/Layout';
 import  { Redirect } from 'react-router-dom';
 
 export default function Home() {
-  return <Redirect to='/CoreDAO-Docs/docs/intro' />;
-}
+  return <Redirect to='/docs/intro' />;
