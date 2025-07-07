3d8e231932544d8bd764170e23ff8e7a88ff50b8
MarryamZulfiqar <13319389+MarryamZulfiqar@users.noreply.github.com>
2025-07-07 22:19:17 +0500

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   // baseUrl: '/',
-  baseUrl: "/",
+  baseUrl: "/CoreDAO-Docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
