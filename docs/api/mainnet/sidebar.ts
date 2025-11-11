36bb15b1a41e0158c3f9477c4bc5826c6de88dd1
MarryamZulfiqar <maryam_xulfiqar@hotmail.com>
2025-11-11 18:30:00 +0300

    added get-token-holder-count-by-contract-address API endpoint

--- docs/api/mainnet/sidebar.ts
+++ docs/api/mainnet/sidebar.ts
@@ -3,6 +3,19 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebar: SidebarsConfig = {
   apisidebar: [
     {
+
+      type: "category",
+      label: "Tokens",
+      items: [
+        {
+          type: "doc",
+          id: "api/mainnet/get-token-holder-count-by-contract-address",
+          label: "Get Token Holder Count by Contract Address",
+          className: "api-method get",
+        },
+      ]
+    },
+    {
       type: "category",
       label: "Accounts",
       items: [
